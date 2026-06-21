6e262581c2ce8b620bfec19328b969a95fc8a557
acpamitpathak92 <amitpathak.azure01@gmail.com>
2026-06-21 16:38:33 +0530

    second commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,8 +21,13 @@ try:
     app = FastAPI(title="AI Travel Planner & Visa Assistant", version="0.1.0")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-        allow_methods=["*"], allow_headers=["*"],
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+        # CORSMiddleware,
+        # allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        # allow_methods=["*"], allow_headers=["*"],
     )
 
     @app.get("/health")
